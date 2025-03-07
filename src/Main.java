dd81f0f69a605621de461613fdb72adc5e6656d8
dylan <jdjjoyner@gmail.com>
2025-03-07 07:40:44 -0600

    Changed redundant code and mad the table viewable in the gui

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
 import java.awt.*;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -13,25 +12,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String contents = Files.readString(Path.of("nobel_prizes.csv"), StandardCharsets.UTF_8);
 
-        List<String> lines = List.of(contents.split("\n"));
+        List<String> lines = contents.lines().toList();
+        String[] headers = lines.get(0).split(",");
+
         ArrayList<Object[]> data = new ArrayList<>();
-        data = lines.stream()
-                .skip(1)
-                .map(line -> line.split(","))
-                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
-        Object[][] allData = data.toArray(new Object[data.size()][]);
 
-        TableModel model = new DefaultTableModel(allData, 4);
+        for (int i =1; i < lines.size(); i++){
+            data.add(lines.get(i).split(","));
+        }
 
+        Object[][] allData = data.toArray(new Object[0][]);
+
+       // TableModel model = new DefaultTableModel(allData, headers);
+        DefaultTableModel model = new DefaultTableModel(allData, headers);
         JTable table = new JTable(model);
+        JScrollPane scrollPane = new JScrollPane(table);
 
-        JPanel panel = new JPanel();
-        panel.add(table);
-        JFrame frame = new JFrame();
+
+        JFrame frame = new JFrame("Nobel Prizes");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(panel, BorderLayout.CENTER);
-        frame.pack();
+        frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
         frame.setVisible(true);
+
 
     }
 }
